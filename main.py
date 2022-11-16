18c8acd3f16bbb523b7ca2753c9e92a24d492406
m98abas <mahmoud.ud@gmail.com>
2022-11-16 08:34:43 +0300

    analize data

--- main.py
+++ main.py
@@ -15,24 +15,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-"""
-    def read_return_data(data_file):
-    # Read file
-    dfCodes = pd.read_csv("codes.csv")
-    datafetched = pd.read_csv(data_file)
-    # group data
-    df1 = datafetched.groupby('ReturnCode').groups
-    keys = df1.keys()
-    sumation = datafetched.pivot_table(index = ['ReturnCode'], aggfunc ='size')
-    sumation = dict(sumation)
-    result_dict = []
-    for i in keys:
-        code_query_result = dfCodes.query('code_result == @i')
-        code_query_result = code_query_result.values[0].tolist()[1]
-        result_dict.append(tuple([i,code_query_result,sumation[i]]))
-    return result_data"""
-
-
 @app.get("/")
 async def root():
     return {"message": "Send data to /upload rather then this "}
@@ -54,20 +36,25 @@ async def result_data(file: UploadFile = File(...)):
     df1 = datafetched.groupby('ReturnCode').groups
     keys = tuple(df1.keys())
 
-    sumation = datafetched.pivot_table(index = ['ReturnCode'], aggfunc ='size')
-    sumation = list(sumation.values)
+    sumation_return_code = datafetched.pivot_table(index = ['ReturnCode'], aggfunc ='size')
+    sumation_return_code = list(sumation_return_code.values)
+
     result_dict = []
+
+    # df = pd.DataFrame(df)
+    datafetched['RequestTimestamp'] = pd.to_datetime(datafetched["RequestTimestamp"]).dt.date
+    datafetched = pd.DataFrame(datafetched.groupby(by=["ReturnCode","RequestTimestamp","Bin"],as_index=False)["ReturnCode"].agg({'count':'count'}))
 
     for i in range(0,len(keys)):
         keypalce = keys[i]
         code_query_result = dfCodes.query('code_result == @keypalce')
         code_query_result = code_query_result.values[0].tolist()[1]
 
-        result_dict.append(tuple([keypalce,code_query_result,sumation[i]]))
+        result_dict.append(tuple([keypalce,code_query_result,sumation_return_code[i]]))
 
-    df1 = pd.DataFrame(result_dict)
+    df1 = pd.DataFrame(result_dict,columns=['response_code','response_description','Count'])
     headers = {'Content-Disposition': 'attachment; filename="data.csv"'}
-    return Response(df1.to_csv(), headers=headers, media_type="text/csv")
+    return Response(datafetched.to_csv(), headers=headers, media_type="text/csv")
 
 
     # return JSONResponse(content=jsonable_encoder(df1.to_json()))
