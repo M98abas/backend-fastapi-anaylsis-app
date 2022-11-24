aab08f4e28c5d8cf4152c4968acbc968fff5dbe3
m98abas <mahmoud.ud@gmail.com>
2022-11-24 14:28:00 +0300

    add-analize-csv-feature

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ async def result_data(file: UploadFile = File(...)):
     datafetched = pd.read_csv(data)
     # group data
     df1 = datafetched.groupby('ReturnCode').groups
-    df2 = pd.DataFrame(datafetched.groupby(['ReturnCode','Bin','AccountNumberLast4','UniqueId','AccountHolder','RequestTimestamp','ChannelName','Currency','Credit','ConnectorDetails','Brand'],as_index=False)['ReturnCode'].agg({'count':'count'}))
-
+    df2 = pd.DataFrame(datafetched.groupby(['ReturnCode','AccountNumberLast4','UniqueId','AccountHolder','RequestTimestamp','ChannelName','Currency','Credit','ConnectorDetails'],as_index=False)['Bin'].agg({'count':'count'}))
+    print(df2)
     keys = tuple(df1.keys())
 
     sumation_return_code = datafetched.pivot_table(index = ['ReturnCode'], aggfunc ='size')
@@ -60,28 +60,45 @@ async def result_data(file: UploadFile = File(...)):
     df1 = pd.DataFrame(result_dict,columns=['key','response_code','response_description','count','children'])
 
     df =df1.to_json(orient='records')
-    # headers = {'Content-Disposition': 'attachment; filename="data.csv"'}
     return Response(df, media_type="text/json")
 
 
+    # headers = {'Content-Disposition': 'attachment; filename="data.csv"'}
     # return JSONResponse(content=jsonable_encoder(df1.to_json()))
 
 @app.post("/analize")
 async def result_data(file: UploadFile = File(...)):
-    # print(request)
-    # Recive file
+      # Recive file
     contents = file.file.read()
     # Convert data in the file
     data = BytesIO(contents)
     # Make the function start working rather than to set up without doing anything
      # Read file
-    df = pd.read_csv(data)
-    df['RequestTimestamp'] = pd.to_datetime(df["RequestTimestamp"]).dt.date
-    pd.set_option('display.max.columns', None)
-    # try:
+    dfCodes = pd.read_csv("codes.csv")
+
+    datafetched = pd.read_csv(data)
+
+    # group data
+    df1 = datafetched.groupby('ReturnCode').groups
+    keys = tuple(df1.keys())
+
+    sumation_return_code = datafetched.pivot_table(index = ['ReturnCode'], aggfunc ='size')
+    sumation_return_code = list(sumation_return_code.values)
+
+    result_dict = []
+
     # df = pd.DataFrame(df)
-    df = pd.DataFrame(df.groupby(by=["Bin","ReturnCode"],as_index=False)["ReturnCode"].agg({'count':'count'}))
-    # df
-    df = df.to_json(orient='records')
-    # headers = {'Content-Disposition': 'attachment; filename="data.csv"'}
-    return Response(df, media_type="text/json")
+    datafetched['RequestTimestamp'] = pd.to_datetime(datafetched["RequestTimestamp"]).dt.date
+    datafetched = pd.DataFrame(datafetched.groupby(by=["ReturnCode","RequestTimestamp","Bin"],as_index=False)["ReturnCode"].agg({'count':'count'}))
+
+    for i in range(0,len(keys)):
+        keypalce = keys[i]
+        code_query_result = dfCodes.query('code_result == @keypalce')
+        code_query_result = code_query_result.values[0].tolist()[1]
+
+        result_dict.append(tuple([keypalce,code_query_result,sumation_return_code[i]]))
+
+    df1 = pd.DataFrame(result_dict,columns=['response_code','response_description','Count'])
+    headers = {'Content-Disposition': 'attachment; filename="data.csv"'}
+    return Response(df1.to_csv(), headers=headers, media_type="text/csv")
+
